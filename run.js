208fd0866c30341c610366b0400230de6d420f55
kidd1118 <kidd1118@users.noreply.github.com>
2016-03-17 10:01:36 +0800

    Create run.js

---
+++ run.js
@@ -0,0 +1,25 @@
+function run (callback){
+  var i = 0;
+  var section = 1;
+  var sectionNumber = 4;
+  var bufferTime = 1000;
+  var total = 100;
+  
+  function func() {
+
+    while (i < section * sectionNumber) {
+      /* exit */
+      if (i >= total) {
+        return;
+      }
+      callback();
+      i++;
+      setTimeout(func, bufferTime);
+    }
+    /* change to next section */
+    if (section * sectionNumber < total) {
+      section++;
+    }
+  }
+  setTimeout(func, bufferTime);
+}
